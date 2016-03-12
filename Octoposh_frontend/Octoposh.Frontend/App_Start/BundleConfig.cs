a2f1fb09e03797e38f586a8959ec8a7256b3f2d7
Dalmiro <dalmiro.granias@gmail.com>
2016-03-12 13:49:41 -0300

    Whole lot of changes
    Added Octokit to Octoposh.Client
    Fixed wording on home text
    Who know what else

--- Octoposh_frontend/Octoposh.Frontend/App_Start/BundleConfig.cs
+++ Octoposh_frontend/Octoposh.Frontend/App_Start/BundleConfig.cs
@@ -9,10 +9,16 @@ namespace Octoposh.Frontend
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/zeroclipboard").Include(
+            "~/Scripts/ZeroClipboard.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/underscore").Include(
+            "~/Scripts/underscore.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -21,7 +27,7 @@ namespace Octoposh.Frontend
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.min.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(                      
                       "~/Content/bootstrap.css",
---
+++ Octoposh_frontend/TestConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new Octokit.Connection(new ProductHeaderValue("DalmiroTesting"));
+            var client = new GitHubClient(connection);
+            var task = client.Repository.Get("Dalmirog", "Octoposh");
+
+            Task.WaitAll(task);
+
+            var repository = task.Result;
+
+            Console.WriteLine(repository.SubscribersCount);
+
+        } 
+        //static async void getcontributors()
+        //{
+        //    var connection = new Octokit.Connection(new ProductHeaderValue("DalmiroTesting"));
+        //    var client = new GitHubClient(connection);
+        //    var repository = client.Repository.Get("octokit", "octokit.net");
+        //    Console.WriteLine(repository.FullName);
+        //}
+    }
+}

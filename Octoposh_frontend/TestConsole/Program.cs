49907c22495d93dab51083f673b277b625f80e5d
Dalmiro <dalmiro.granias@gmail.com>
2016-03-12 21:55:18 -0300

    Contributors working example

--- Octoposh_frontend/TestConsole/Program.cs
+++ Octoposh_frontend/TestConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Octokit;
+using Octoposh.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,15 +13,45 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            var connection = new Octokit.Connection(new ProductHeaderValue("DalmiroTesting"));
-            var client = new GitHubClient(connection);
-            var task = client.Repository.Get("Dalmirog", "Octoposh");
+            var credentials = new Octokit.Credentials("dalmirog", "somepassword");
+            var APIconnection = new ApiConnection(new Octokit.Connection(new ProductHeaderValue("DalmiroTesting"))
+            {
+                Credentials = credentials
+            });
+            //var APIconnection = new ApiConnection(new Octokit.Connection(new ProductHeaderValue("DalmiroTesting")));
+            var repositoryCommitClient = new RepositoryCommitsClient(APIconnection);
+                    
+            var task = repositoryCommitClient.GetAll("Dalmirog", "Octoposh");            
 
             Task.WaitAll(task);
 
-            var repository = task.Result;
+            var Contributors = new List<GithubContributor>();
 
-            Console.WriteLine(repository.SubscribersCount);
+            foreach(var commit in task.Result)
+            {
+                if (Contributors.Exists(x => x.Username == commit.Committer.Login))
+                {
+                    Contributors.Find(x => x.Username == commit.Committer.Login).Commits++;
+                }
+                else
+                {
+                    GithubContributor contributor = new GithubContributor()
+                    {
+                        Username = commit.Committer.Login,
+                        AvatarURL = commit.Committer.AvatarUrl,
+                        Commits = 0,
+                        UserURL = commit.Committer.HtmlUrl
+                    };
+
+                    Contributors.Add(contributor);
+                }
+            }
+
+            foreach(var c in Contributors)
+            {
+                Console.WriteLine("User [{0}] commited [{1}]",c.Username ,c.Commits);
+            }    
+            
 
         } 
         //static async void getcontributors()

2138a4c107a3fabdf0d4a536f166b0c8c3716824
Dalmiro <dalmiro.granias@gmail.com>
2016-06-21 01:44:41 -0300

    commenting console app

--- Octoposh_frontend/TestConsole/Program.cs
+++ Octoposh_frontend/TestConsole/Program.cs
@@ -11,49 +11,50 @@ namespace TestConsole
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            var credentials = new Octokit.Credentials("dalmirog", "somepassword");
-            var APIconnection = new ApiConnection(new Octokit.Connection(new ProductHeaderValue("DalmiroTesting"))
-            {
-                Credentials = credentials
-            });
-            //var APIconnection = new ApiConnection(new Octokit.Connection(new ProductHeaderValue("DalmiroTesting")));
-            var repositoryCommitClient = new RepositoryCommitsClient(APIconnection);
+        //static void Main(string[] args)
+        //{
+        //    <#
+        //    var credentials = new Octokit.Credentials("dalmirog", "somepassword");
+        //    var APIconnection = new ApiConnection(new Octokit.Connection(new ProductHeaderValue("DalmiroTesting"))
+        //    {
+        //        Credentials = credentials
+        //    });
+        //    //var APIconnection = new ApiConnection(new Octokit.Connection(new ProductHeaderValue("DalmiroTesting")));
+        //    var repositoryCommitClient = new RepositoryCommitsClient(APIconnection);
                     
-            var task = repositoryCommitClient.GetAll("Dalmirog", "Octoposh");            
+        //    var task = repositoryCommitClient.GetAll("Dalmirog", "Octoposh");            
 
-            Task.WaitAll(task);
+        //    Task.WaitAll(task);
 
-            var Contributors = new List<GithubContributor>();
+        //    var Contributors = new List<GithubContributor>();
 
-            foreach(var commit in task.Result)
-            {
-                if (Contributors.Exists(x => x.Username == commit.Committer.Login))
-                {
-                    Contributors.Find(x => x.Username == commit.Committer.Login).Commits++;
-                }
-                else
-                {
-                    GithubContributor contributor = new GithubContributor()
-                    {
-                        Username = commit.Committer.Login,
-                        AvatarURL = commit.Committer.AvatarUrl,
-                        Commits = 0,
-                        UserURL = commit.Committer.HtmlUrl
-                    };
+        //    foreach(var commit in task.Result)
+        //    {
+        //        if (Contributors.Exists(x => x.Username == commit.Committer.Login))
+        //        {
+        //            Contributors.Find(x => x.Username == commit.Committer.Login).Commits++;
+        //        }
+        //        else
+        //        {
+        //            GithubContributor contributor = new GithubContributor()
+        //            {
+        //                Username = commit.Committer.Login,
+        //                AvatarURL = commit.Committer.AvatarUrl,
+        //                Commits = 0,
+        //                UserURL = commit.Committer.HtmlUrl
+        //            };
 
-                    Contributors.Add(contributor);
-                }
-            }
+        //            Contributors.Add(contributor);
+        //        }
+        //    }
 
-            foreach(var c in Contributors)
-            {
-                Console.WriteLine("User [{0}] commited [{1}]",c.Username ,c.Commits);
-            }    
+        //    foreach(var c in Contributors)
+        //    {
+        //        Console.WriteLine("User [{0}] commited [{1}]",c.Username ,c.Commits);
+        //    }    
             
 
-        } 
+        //} 
         //static async void getcontributors()
         //{
         //    var connection = new Octokit.Connection(new ProductHeaderValue("DalmiroTesting"));
